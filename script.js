a7ea9890e2b6281fa973cc1323639f81cb843447
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-06-02 15:44:10 -0300

    Create the initial square grid structure

---
+++ script.js
@@ -0,0 +1,10 @@
+let gridContainer = document.querySelector("#grid-container");
+
+const SQUARE_SIZE_PX = 32;
+const SQUARES_COUNT = 16*16;
+
+for (let i = 0; i < SQUARES_COUNT; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    gridContainer.appendChild(square);
+}
